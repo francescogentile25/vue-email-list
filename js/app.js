48dcda66cf2adadeec23de68bd6ef8eded32d40e
francescogentile25 <francescogentile250893@gmail.com>
2023-02-27 16:00:07 +0100

    ERROR

--- js/app.js
+++ js/app.js
@@ -11,40 +11,14 @@ createApp({
             axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((res) => {
-                this.mail.push(res.data.response)
+                this.mail=res.data.response
             })
         },
     }, 
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.fetchMail()
+    }
+    },
 }).mount('#app')
 
-
-
-// const { createApp } = Vue
-
-// // console.log(axios)
-
-// createApp({
-// 	data() {
-// 		return {
-// 			name: 'Gianni',
-// 			names: [],
-// 		}
-// 	},
-// 	methods: {
-// 		fetchName() {
-// 			axios
-// 				.get('https://flynn.boolean.careers/exercises/api/random/name')
-// 				.then((res) => {
-// 					// res  = risposta del resrver
-// 					// res.data = dati contenuti nella risposta
-// 					console.log(res.data.response)
-// 					this.names.push(res.data.response)
-// 				})
-// 		},
-// 	},
-// 	mounted() {
-// 		for (let i = 0; i < 10; i++) {
-// 			this.fetchName()
-// 		}
-// 	},
-// }).mount('#app')
